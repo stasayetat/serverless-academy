a4c44d7486b58b57691b1fc2831656630ba2a562
Stanislaw <stas.yarets234@gmail.com>
2023-06-01 16:33:05 +0300

    Create function for set value in sortMap

--- 01_cli_interactive_sort/app.js
+++ 01_cli_interactive_sort/app.js
@@ -7,68 +7,7 @@ const rl = createInterface({
 });
 const question = promisify(rl.question).bind(rl);
 const sortMap = new Map();
-sortMap.set(1, (args)=> {
-    const allWords = args.filter((item)=> {
-        if(!Number(item) && item !== '0') {
-            return true;
-        }
-    });
-    allWords.sort();
-    console.log(allWords);
-});
-
-sortMap.set(2, (args)=> {
-    const allNums = args.filter((item)=> {
-        if(Number(item) || item === '0') {
-            return true;
-        }
-    });
-    allNums.sort((a, b)=> a - b);
-    console.log(allNums);
-});
-
-sortMap.set(3, (args)=> {
-    const allNums = args.filter((item)=> {
-        if(Number(item) || item === '0') {
-            return true;
-        }
-    });
-    allNums.sort((a, b)=> b - a);
-    console.log(allNums);
-});
-
-sortMap.set(4, (args)=> {
-    const allWords = args.filter((item)=> {
-        if(!Number(item) && item !== '0') {
-            return true;
-        }
-    });
-    allWords.sort((a, b)=> {
-        if(a.length > b.length)
-            return 1;
-        if(a.length < b.length)
-            return -1;
-        return 0;
-    });
-    console.log(allWords);
-});
-
-sortMap.set(5, (args)=> {
-    const allWords = args.filter((item)=> {
-        if(!Number(item) && item !== '0') {
-            return true;
-        }
-    });
-    const wordSet = Array.from(new Set(allWords));
-    console.log(wordSet);
-});
-
-sortMap.set(6, (args)=> {
-    const wordSet = Array.from(new Set(args));
-    console.log(wordSet);
-});
-
-
+setValueForSortMap();
 
 while(true) {
     const answer = await question("\n\nEnter a few words or numbers separated by spaces: ");
@@ -103,4 +42,67 @@ async function getSortType() {
             return Number(answerSortType);
         }
     }
+}
+
+function setValueForSortMap() {
+    sortMap.set(1, (args)=> {
+        const allWords = args.filter((item)=> {
+            if(!Number(item) && item !== '0') {
+                return true;
+            }
+        });
+        allWords.sort();
+        console.log(allWords);
+    });
+
+    sortMap.set(2, (args)=> {
+        const allNums = args.filter((item)=> {
+            if(Number(item) || item === '0') {
+                return true;
+            }
+        });
+        allNums.sort((a, b)=> a - b);
+        console.log(allNums);
+    });
+
+    sortMap.set(3, (args)=> {
+        const allNums = args.filter((item)=> {
+            if(Number(item) || item === '0') {
+                return true;
+            }
+        });
+        allNums.sort((a, b)=> b - a);
+        console.log(allNums);
+    });
+
+    sortMap.set(4, (args)=> {
+        const allWords = args.filter((item)=> {
+            if(!Number(item) && item !== '0') {
+                return true;
+            }
+        });
+        allWords.sort((a, b)=> {
+            if(a.length > b.length)
+                return 1;
+            if(a.length < b.length)
+                return -1;
+            return 0;
+        });
+        console.log(allWords);
+    });
+
+    sortMap.set(5, (args)=> {
+        const allWords = args.filter((item)=> {
+            if(!Number(item) && item !== '0') {
+                return true;
+            }
+        });
+        const wordSet = Array.from(new Set(allWords));
+        console.log(wordSet);
+    });
+
+    sortMap.set(6, (args)=> {
+        const wordSet = Array.from(new Set(args));
+        console.log(wordSet);
+    });
 }
